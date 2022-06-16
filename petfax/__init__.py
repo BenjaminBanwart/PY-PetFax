aa719b0bb68f1fe2d5f1fcb006f9c08bac81a267
Benjamin Banwart <benjibanwart@gmail.com>
2022-06-15 21:29:38 -0500

    added SQL database

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,11 +1,19 @@
 from flask import Flask
+from flask_migrate import Migrate
 
 def create_app():
     app = Flask(__name__)
 
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1303function(){};@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False             
+
     @app.route('/')
     def hello():
         return 'Hello, PetFax!'
+
+    from . import models
+    models.db.init_app(app)
+    migrate = Migrate(app, models.db)
     
     from . import pet
     app.register_blueprint(pet.bp)
